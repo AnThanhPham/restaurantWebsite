9b9b8af4196717572b6d588c0bf82fc3be604120
Tien_Chung_dang_code <tiencchung2612@gmail.com>
2023-10-11 16:52:36 +0700

    first commit

--- assets/js/contact.js
+++
@@ -1,7 +0,0 @@
-let button = document.getElementsByTagName('button')[0]
-button.addEventListener('click',()=>{
-    alert(`Your review has been submitted`)
-    document.getElementById("name").value=``
-    document.getElementById("email").value=``
-    document.getElementById("review").value=``
-})

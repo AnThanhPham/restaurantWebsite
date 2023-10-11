466304f0f9dac6ac54ef83f3ef6c2852e1c3340b
Tien_Chung_dang_code <138287457+TiennChung@users.noreply.github.com>
2023-10-11 17:36:20 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dự án Website quản lí nhà hàng bán đồ ăn uống
+
+## Thành viên nhóm 7 CNTT3:
+- Phạm Thành An
+- Hoàng Trọng An
+- Nguyễn Phương Nam
+- Lê Tiến Chung
+
+## Miêu tả dự án:
---
+++ assets/js/contact.js
@@ -0,0 +1,7 @@
+let button = document.getElementsByTagName("button")[0];
+button.addEventListener("click", () => {
+  alert(`Your review has been submitted`);
+  document.getElementById("name").value = ``;
+  document.getElementById("email").value = ``;
+  document.getElementById("review").value = ``;
+});
